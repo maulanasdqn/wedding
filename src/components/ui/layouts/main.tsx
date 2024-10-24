affe5114be80c2268b8bf9a1a549ee0aad0dee75
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2024-10-24 23:56:43 +0700

    chore: add splash screen

--- src/components/ui/layouts/main.tsx
+++ src/components/ui/layouts/main.tsx
@@ -1,12 +1,28 @@
-import type { FC, ReactElement } from "react";
+import { useEffect, useState, type FC, type ReactElement } from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "@/components/ui/navbar";
+import { SplashScreen } from "../splash";
 
 export const MainLayout: FC = (): ReactElement => {
+  const loadTimeOut = 4;
+  const [loadPage, setLoadPage] = useState(false);
+
+  const parseLoad = parseInt(`${loadTimeOut / 2}000`);
+
+  useEffect(() => {
+    setTimeout(() => setLoadPage(true), parseLoad);
+  }, [loadPage, setLoadPage, parseLoad]);
+
   return (
     <main className="flex flex-col items-start justify-start py-2 md:py-6 px-6 md:px-8 lg:px-20 min-h-screen bg-primary">
-      <Navbar />
-      <Outlet />
+      {loadPage ? (
+        <>
+          <Navbar />
+          <Outlet />
+        </>
+      ) : (
+        <SplashScreen loadTimeOut={loadTimeOut} />
+      )}
     </main>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,8 @@ const config: Config = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: ["class", '[data-mode="dark"]'],
   theme: {
+    colors,
     extend: {
-      colors,
       fontFamily,
     },
   },
